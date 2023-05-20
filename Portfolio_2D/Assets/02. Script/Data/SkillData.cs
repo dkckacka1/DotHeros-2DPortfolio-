d34e55ebc1c23902c0cea2c70c2a85411a7e00d9
dkckacka1 <125544460+dkckacka1@users.noreply.github.com>
2023-05-20 20:14:31 +0900

    Kimhawon
    스킬 데이타 작성

---
+++ Portfolio_2D/Assets/02. Script/Data/SkillData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Portfolio
+{
+    public class SkillData
+    {
+        public int ID;
+        public string skillName;
+        public string skillDesc;
+    }
+}
