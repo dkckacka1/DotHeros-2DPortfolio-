0472b0ad34620dfb9e2e03559b12f4c10b8f5f4b
dkckacka1 <125544460+dkckacka1@users.noreply.github.com>
2023-06-12 10:53:59 +0900

    Kimhawon
     상점 UI 구성

--- Portfolio_2D/Assets/02. Script/Lobby/UI/ShopPanel/ShopPanel.cs
+++ Portfolio_2D/Assets/02. Script/Lobby/UI/ShopPanel/ShopPanel.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Portfolio.Lobby
 {
     public class ShopPanel : PanelUI
     {
+        [SerializeField] ScrollRect diaShopScrollRect;
+        [SerializeField] ScrollRect goldShopScrollRect;
+
+
         protected override void OnEnable()
         {
             base.OnEnable();
+        }
+
+        public void ShowDiaShop(bool isActive)
+        {
+            diaShopScrollRect.gameObject.SetActive(isActive);
+        }
+
+        public void ShowGoldShop(bool isActive)
+        {
+            goldShopScrollRect.gameObject.SetActive(isActive);
         }
     }
 }
